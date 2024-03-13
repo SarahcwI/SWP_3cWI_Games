306c0ceb2abd4e74a809aa6f24143d456d027eea
Sarah <sarah.wolf@htldornbirn.at>
2024-03-13 09:04:30 +0100

    rectangle_moving

--- src/at/sarah/games/firstgame/Rectangles.java
+++ src/at/sarah/games/firstgame/Rectangles.java
@@ -5,6 +5,7 @@ import org.newdawn.slick.tests.AnimationTest;
 
 public class Rectangles extends BasicGame {
     private int x;
+    private int y;
     public Rectangles(String title) {
         super(title);
     }
@@ -12,23 +13,30 @@ public class Rectangles extends BasicGame {
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
         this.x = 100;
+        //this.y = 100;
     }
 
     @Override
     public void update(GameContainer gameContainer, int i) throws SlickException {
         this.x++;
+        this.y++;
+
+        if (this.x>800){
+            this.x=0;
+            this.y=0;
+        }
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        graphics.drawRect(this.x,100,100,100);
+        graphics.drawRect(this.x,this.y,100,100);
         graphics.drawString("Hello World!", 50, 50);
     }
 
     public static void main(String[] argv) {
         try {
             AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
-            container.setDisplayMode(800,600,false);
+            container.setDisplayMode(1000,800,false);
             container.start();
         } catch (SlickException e) {
             e.printStackTrace();
